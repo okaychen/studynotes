f8337ce612d410c602367d025fc19671a4fafdfd
okaychen <1440281580@qq.com>
2019-06-27 22:56:46 +0800

    add new file

---
+++ vue/vue-learningCode/vueComponent.js
@@ -0,0 +1,24 @@
+Vue.component('v-header', {
+    props: [],
+    template: `
+        <div id="box">{{message}}</div>
+    `,
+    data: function () {
+        return {
+            message: 'hello world'
+        }
+    },
+    mounted() {
+
+    },
+    commuted: {
+
+    },
+    methods: {
+
+    },
+    watch: {
+
+    },
+
+})
