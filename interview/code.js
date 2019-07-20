bdaaf092885fc159006968b555203ac7e7a0df57
okaychen <1440281580@qq.com>
2019-07-20 17:08:01 +0800

    add promise.add

--- interview/code.js
+++ interview/code.js
@@ -5,3 +5,28 @@ function arrSlice(arr) {
         .map((e, i) => i % 2 ? null : [arr[i], arr[i + 1]]) // 两两取出  （i取模2，有余数的话返回null，否者数组中取出两个元素）
         .filter(Boolean)
 }
+
+//promise.all的实现
+function promiseAll(promises) {
+    return new Promise(function (reject, resolve) {
+        if (!isArray(promises)) {
+            return reject(new TypeError('arguments must be an array'));
+        }
+        var resolvedcount = 0;
+        var promiseNum = promises.length;
+        var resolvedValues = new Array(promiseNum);
+        for (var i = 0; i < promiseNum; i++) {
+            (function () {
+                Promise.resolve(promises[i]).then(function (value) {
+                    resolvedcount++;
+                    resolvedValues[i] = value;
+                    if (resolvedcount == promiseNum) {
+                        return resolve(resolvedValues)
+                    }
+                }, function (reason) {
+                    reject(reason);
+                })
+            })(i)
+        }
+    })
+}
